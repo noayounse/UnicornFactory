e2b1b72f9fea30f9ecb515ef9ba57ab3a842bef2
noa <nyounse@gmail.com>
2015-12-28 15:38:46 -0800

    erased

--- 2015_10 redux/assets/getFooter.js
+++
@@ -1,2 +0,0 @@
-var div = document.getElementById("footer");
-div.innerHTML = '<footer class="footer"><div class="container">	<p class="text-muted">© Noa Younse&nbsp;&nbsp; 2016</p>	</div></footer>';
