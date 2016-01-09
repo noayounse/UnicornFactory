945c8eb10cd8bee7017b52c3c6bd6361e4c00978
noa <nyounse@gmail.com>
2016-01-09 10:04:12 -0500

    update

--- assets/getTopHomeLink.js
+++
@@ -1,3 +0,0 @@
-// for the project pages this will simply return the link back home
-var div = document.getElementById("topHomeLink");
-div.innerHTML = '<a href="../../index.html" class="mainProjectTitleBackground">< home</a>';
