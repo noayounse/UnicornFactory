07bcdc855c3998dcda2eec03e3d532c4a7f41018
noa <nyounse@gmail.com>
2016-01-09 11:26:03 -0500

    update

--- js/getHeader.js
+++ js/getHeader.js
@@ -12,7 +12,16 @@ var homeLinkHeight = 0;
 var image_url = $('.projectBackground').css('background-image'),
 	image;
 
-console.log("in getHeadera the top.  got the image_url at: " + image_url);	
+console.log("in getHeadera the top.  got the image_url at: " + image_url);
+
+getImageSize($("#quickSizer"), function(width, height) {
+	//alert('size is: ' + width + 'x' + height);
+	projectBackgroundHeight = height;
+	jumboHeight = height;
+	projectBackgroundWidth = width;
+	parallax();
+});
+
 
 // Remove url() or in case of Chrome url("")
 image_url = image_url.match(/^url\("?(.+?)"?\)$/);
@@ -25,8 +34,8 @@ if (image_url[1]) {
 	$(image).load(function() {
 		//alert(image.width + 'x' + image.height);
 		console.log("in getHeader.  height of projectBackground div is: " + $(".projectBackground").height());
-		projectBackgroundHeight = image.height;
-		projectBackgroundWidth = image.width;
+		//projectBackgroundHeight = image.height;
+		//projectBackgroundWidth = image.width;
 		displayWidth = $(window).width();
 		console.log("projectBackground is: " + projectBackgroundHeight);
 		console.log("dims of the background image as: " + projectBackgroundHeight + " high by " + projectBackgroundWidth + " wide");
@@ -50,28 +59,7 @@ if (image_url[1]) {
 		homeLinkHeight = $('.homeLink').height();
 		//console.log("home link height as: " + homeLinkHeight);
 
-		function parallax() {
-			var scrolled = $(window).scrollTop();
-			var newHeight = jumboHeight - scrolled; // used to calculate the home link top
-			//console.log(newHeight);
-			leftSide = $('.projectTitle').offset().left;
 
-
-			var newTop = -scrolled * .85; // controls the speed that it goes up
-
-
-			if (newTop < (-scrolled)) newTop = -scrolled;
-			$('.projectBackground').css('top', newTop + 'px');
-
-			var homeLinkTop = 10;
-			if (newHeight < homeLinkTop + homeLinkHeight) {
-				homeLinkTop = newHeight - 0 * homeLinkTop - homeLinkHeight;
-			}
-			$('.homeLink').css({
-				top: homeLinkTop + 'px',
-				left: leftSide + 'px'
-			});
-		}
 
 		$(window).scroll(function(e) {
 			parallax();
@@ -82,5 +70,64 @@ if (image_url[1]) {
 		parallax();
 
 	});
-	image.src = image_url;
+image.src = image_url;
+}
+
+
+function parallax() {
+	var scrolled = $(window).scrollTop();
+	var newHeight = jumboHeight - scrolled; // used to calculate the home link top
+	//console.log(newHeight);
+	leftSide = $('.projectTitle').offset().left;
+
+	var newTop = -scrolled * .85; // controls the speed that it goes up
+
+
+	if (newTop < (-scrolled)) newTop = -scrolled;
+	$('.projectBackground').css('top', newTop + 'px');
+
+	var homeLinkTop = 10;
+	if (newHeight < homeLinkTop + homeLinkHeight) {
+		homeLinkTop = newHeight - 0 * homeLinkTop - homeLinkHeight;
+	}
+	$('.homeLink').css({
+		top: homeLinkTop + 'px',
+		left: leftSide + 'px'
+	});
+}
+
+
+
+// from http://stackoverflow.com/questions/23390393/get-image-height-before-its-fully-loaded
+function getImageSize(img, callback) {
+	img = $(img);
+
+	var wait = setInterval(function() {
+		var w = img.width(),
+			h = img.height();
+
+		if (w && h) {
+			done(w, h);
+		}
+	}, 0);
+
+	var onLoad;
+	img.on('load', onLoad = function() {
+		done(img.width(), img.height());
+	});
+
+
+	var isDone = false;
+
+	function done() {
+		if (isDone) {
+			return;
+		}
+		isDone = true;
+
+		clearInterval(wait);
+		img.off('load', onLoad);
+
+		callback.apply(this, arguments);
+	}
 }
